df31c0baa84e462537ba47b3d6dccecc86c35a1d
mkmovieshu <ipsappdevelopers@gmail.com>
2026-07-10 23:12:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002312203253'))
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' -1002336864238 -1002344843738 -1001543897566 -1003298840050 -1001673585958 -1001677901864 -1001556163470').split()]  # For Multiple Id Use One Space Between Each.
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', ' -1002336864238 -1002344843738 -1001543897566 -1003298840050 -1001673585958 -1001677901864 -1001556163470 -1002156433450').split()]  # For Multiple Id Use One Space Between Each.
 
 # auth_channel means force subscribe channel.
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
